56ebe36ad57e9d7605e29fea302b51807e967465
Joshua Williams <com.joshua360@gmail.com>
2022-03-07 21:37:43 -0600

    feat(aws-cli): add default aws_account_id and aws_region value from aws credentials if not passed in as parameters

--- docker/aws-ecr-authenticate.sh
+++ docker/aws-ecr-authenticate.sh
@@ -4,12 +4,11 @@
 aws_account_id=$1
 aws_region=$2
 if [ -z $aws_account_id ]; then
-  echo "Please enter AWS account id"
-  exit 0
+  aws_account_id=$(aws sts get-caller-identity --query "Account" --output text)
 fi
 
 if [ -z $aws_region ]; then
-  aws_region="us-east-2"
+  aws_region=$(aws configure get region)
 fi
 aws_registry_url="${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com"
 aws_token=$(aws ecr get-login-password --region $aws_region)
