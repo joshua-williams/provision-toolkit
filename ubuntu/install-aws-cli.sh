64e7cf334a78480540e079f996540b8060142e21
Joshua Williams <com.joshua360@gmail.com>
2022-03-07 21:19:18 -0600

    chore(aws-cli): update aws-cli install script

--- ubuntu/install-aws-cli.sh
+++ ubuntu/install-aws-cli.sh
@@ -1,5 +1,14 @@
+# Install zip and unzip
+apt-get install zip unzip
+# Create bin directory to install aws cli to
 [ ! -d $HOME/bin ] && mkdir $HOME/bin
+# Add bin directory to user path variable
 echo 'PATH=~/bin:$PATH' >> ~/.bashrc
+# Download aws-cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# Unarchive aws-cli
 unzip awscliv2.zip
-sudo ./aws/install -i $HOME/bin
+# Install aws-cli into previously create bin directory
+sudo ./aws/install -i ~/bin -b ~/bin
+# Remove temporary aws directory
+rm -rf ~/aws
