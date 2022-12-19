bcb1696848003cb108ebe45127a4ff930ad43b72
Joshua Williams <com.joshua360@gmail.com>
2022-12-19 09:37:13 -0600

    chore(lemp): add felix user to sudo group on lemp stack setup

--- ubuntu/22/lemp.sh
+++ ubuntu/22/lemp.sh
@@ -12,3 +12,7 @@ apt install php8.1-fpm php-mysql -y
 # add non-root user
 useradd -m -s /bin/bash -G sudo felix
 echo "felix ALL=(ALL:ALL) ALL" > /etc/sudoers.d/felix
+# Create felix mysql user
+mysql -e "CREATE USER 'felix'@'localhost'"
+mysql -e "GRANT ALL ON *.* TO 'felix'@'localhost'"
+mysql -e "FLUSH PRIVILEGES"
